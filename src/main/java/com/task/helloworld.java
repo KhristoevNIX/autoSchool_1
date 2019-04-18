7af323796e42187e194ff4a8bf6245508d584ae6
khristoev <aleksandr.khristoiev@nixsolutions.com>
2019-04-18 13:27:56 +0300

    Fixed first test parameters

---
+++ src/main/java/com/task/helloworld.java
@@ -0,0 +1,19 @@
+package com.task;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World :)");
+        name();
+    }
+
+    public static void name(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + " :)");
+    }
+}
